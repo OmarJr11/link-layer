4328867e05395af8ae89d2a1a96573acff429bf4
Omar <omaryoeljr@gmail.com>
2023-11-06 18:23:08 -0400

    :art: develop feat(Hamming) Comment class

--- classes/hamming.py
+++ classes/hamming.py
@@ -1,3 +1,15 @@
+"""
+    Clase Hamming
+    Contará con las variables:
+        isVerification = Booleano indica si el bit es de verificacion o mensaje
+        position =  Entero que indica la posicion del bit
+        bit =  0 o 1
+        pow =  Array de Enteros que indica la descomposicion de la posicion del bit en 2**n
+               Es decir la posicion 5 = [4(2**2), 1(2**1)]
+    Para crear pow de la posicion se envia una lista de 2**n  
+"""
+
+
 class Hamming:
     def __init__(self, isVerification: bool, position: int, bit, listPow):
         self.isVerification = isVerification
